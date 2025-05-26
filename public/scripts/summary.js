7ba6f6adb857bd335171e9b9984381d351f297fe
OGURA Kazuki <116713488+oguguogu@users.noreply.github.com>
2025-05-26 19:45:06 +0900

    Update

--- public/scripts/summary.js
+++ public/scripts/summary.js
@@ -1,4 +1,4 @@
-const map = L.map("map").setView([35.6368549, 139.533389], 12);
+const map = L.map("map").setView([0, 0], 12);
 
 // 背景地図はOpenStreetMap
 const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
