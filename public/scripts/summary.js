b3bdefadaddf2b7f417c568ba592a230cdfdc8f7
OGURA Kazuki <116713488+oguguogu@users.noreply.github.com>
2025-05-27 22:29:33 +0900

    Update

--- public/scripts/summary.js
+++ public/scripts/summary.js
@@ -106,13 +106,13 @@ Promise.all([getAreaList(), getProgress(), getProgressCountdown()])
           }
 
           const polygon = L.geoJSON(feature, {
-            style: getGeoJsonStyle(progress[areaName]),//key
+            style: getGeoJsonStyle(progress[key]),//key
           });
 
           polygon.bindPopup(`
             <b>${areaName}</b><br>
-            ポスター貼り進捗: ${(progress[areaName]*100).toFixed(1)}%<br> 
-            残り: ${progressCountdown[areaName]}ヶ所
+            ポスター貼り進捗: ${(progress[key]*100).toFixed(1)}%<br> 
+            残り: ${progressCountdown[key]}ヶ所
           `);// key
 
           polygon.addTo(map);
